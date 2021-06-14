9e4a44fd777c375bac51836c0d467820de3205ca
nienkevonk <84348574+nienkevonk@users.noreply.github.com>
2021-06-14 19:10:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const age = "100";
+if (age >= 18) {
+    console.log("Je mag naar binnen!");
+} else {
+    console.log("Je bent helaas te jong. Blijf buiten s.v.p.");
+}
+
+const isFemale = false;
+if (isFemale) {
+    console.log("Je bent een vrouw!");
+} else {
+    console.log("Je bent een man.");
+}
+
+const driverStatus = "Bob";
+if (driverStatus == "Bob") {
+    console.log("Je bent de BOB, je mag rijden");
+} else {
+    console.log("Je mag niet meer rijden!!");
+}
